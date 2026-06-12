f7e04262cbec29d04671fb61e5cb5a7f36034350
Nikola Ivačič <nikola.ivacic@dropchop.com>
2026-06-12 09:00:32 +0200

    wip

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import torch
+
+from transformers import pipeline
+
+torch.set_default_device("cuda")
+
 def main():
+    p = pipeline("text-generation",
+                        model="Qwen/Qwen3-4B-Instruct-2507",
+                        dtype=torch.float16, device_map="auto")
+
+    chat = [
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': 'My internet keeps disconnecting'}
+    ]
+
+    for _ in range(3):
+        response = p(chat, max_new_tokens=64, do_sample=True, top_k=10)
+        print(response[0]["generated_text"][-1]["content"])
+
     print("Hello from slaif!")
 
 
